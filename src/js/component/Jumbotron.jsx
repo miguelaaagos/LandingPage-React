cd41ac154a5fddbe1f764d40bd1ba2064150fc19
Miguel Palacios <miguel.palacios03@inacapmail.cl>
2022-09-29 03:20:34 -0300

    main

--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -3,14 +3,12 @@ import PropTypes from "prop-types";
 
 const Jumbotron = (props) => {
     return (
-        <div className="jumbotron jumbotron-fluid m-5 text-center">
-            <div className="container">
+        <div className="jumbotron text-center bg-secondary pb-4">
                 <h1 className="display-4">{props.title}</h1>
                 <p className="lead">{props.description}</p>
                 <a className="btn btn-primary btn-lg" href={props.buttonURL} role="button">
                     {props.buttonLabel}
                 </a>
-            </div>
         </div>
     );
 };
