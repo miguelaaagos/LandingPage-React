67be542d85dd379f7c766d6883c5be708f54214d
Miguel Palacios <miguel.palacios03@inacapmail.cl>
2022-10-05 20:24:28 -0300

    Cambios esteticos realizados

--- src/js/component/Footer.jsx
+++ src/js/component/Footer.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 const Footer = (props) => {
     // 1) replace the hard-coded image, description, link, etc. With their property variable.
     return (
-        <div className='text-center p-4 bg-dark text-white' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        <div className='footer text-center p-4 bg-dark text-white' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
             {props.title}
             <a className='text-reset fw-bold' href='https://mdbootstrap.com/'>
                 {props.siteUrl}
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -11,57 +11,61 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	return (
 		<div className="container-fluid">
+			<section>
+				<div className="row">
+					<Navbar
+						navbarTitle="Start Bootstrap"
+						item1="Home"
+						item2="About"
+						item3="Services"
+						item4="Contact"
+					/>
+				</div>
+				<div className="row">
+					<Jumbotron
+						title="A warm welcome!"
+						description="Lorem Ipsum is simply dummy text of the printing and typesetting industry"
+						buttonLabel="Call to action!"
+					/>
+				</div>
+				<div className="row justify-content-center pt-3">
+					<Card
+						title="Card title"
+						imageUrl="https://images.all-free-download.com/images/graphicwebp/cat_cat_face_cats_eyes_240527.webp"
+						description="Lorem Ipsum is simply dummy text of the printing and typesetting industry"
+						buttonLabel="Find Out More!"
+					/>
+
+					<Card
+						title="Card title"
+						imageUrl="https://images.all-free-download.com/images/graphicwebp/cat_cat_face_cats_eyes_240527.webp"
+						description="Lorem Ipsum is simply dummy text of the printing and typesetting industry"
+						buttonLabel="Find Out More!"
+					/>
+
+					<Card
+						title="Card title"
+						imageUrl="https://images.all-free-download.com/images/graphicwebp/cat_cat_face_cats_eyes_240527.webp"
+						description="Lorem Ipsum is simply dummy text of the printing and typesetting industry"
+						buttonLabel="Find Out More!"
+					/>
+
+					<Card
+						title="Card title"
+						imageUrl="https://images.all-free-download.com/images/graphicwebp/cat_cat_face_cats_eyes_240527.webp"
+						description="Lorem Ipsum is simply dummy text of the printing and typesetting industry"
+						buttonLabel="Find Out More!"
+					/>
+				</div>
+			</section>
 			<div className="row">
-				<Navbar
-					navbarTitle="Start Bootstrap"
-					item1="Home"
-					item2="About"
-					item3="Services"
-					item4="Contact"
-				/>
+				<footer>
+					<Footer
+						title="© 2022 Copyright: "
+						siteUrl="HamburguesasMC.com"
+					/>
+				</footer>
 			</div>
-			<div className="row col-xl-12">
-				<Jumbotron
-					title="A warm welcome!"
-					description="Lorem Ipsum is simply dummy text of the printing and typesetting industry"
-					buttonLabel="Call to action!"
-				/>
-			</div>
-			<div className="row justify-content-center">
-				<Card
-					title="Card title"
-					imageUrl="https://images.all-free-download.com/images/graphicwebp/cat_cat_face_cats_eyes_240527.webp"
-					description="Lorem Ipsum is simply dummy text of the printing and typesetting industry"
-					buttonLabel="Find Out More!"
-				/>
-
-				<Card
-					title="Card title"
-					imageUrl="https://images.all-free-download.com/images/graphicwebp/cat_cat_face_cats_eyes_240527.webp"
-					description="Lorem Ipsum is simply dummy text of the printing and typesetting industry"
-					buttonLabel="Find Out More!"
-				/>
-
-				<Card
-					title="Card title"
-					imageUrl="https://images.all-free-download.com/images/graphicwebp/cat_cat_face_cats_eyes_240527.webp"
-					description="Lorem Ipsum is simply dummy text of the printing and typesetting industry"
-					buttonLabel="Find Out More!"
-				/>
-
-				<Card
-					title="Card title"
-					imageUrl="https://images.all-free-download.com/images/graphicwebp/cat_cat_face_cats_eyes_240527.webp"
-					description="Lorem Ipsum is simply dummy text of the printing and typesetting industry"
-					buttonLabel="Find Out More!"
-				/>
-			</div>
-			<footer>
-				<Footer
-					title="© 2022 Copyright: "
-					siteUrl="HamburguesasMC.com"
-				/>
-			</footer>
 		</div>
 	);
 };
